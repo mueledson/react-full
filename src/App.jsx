b3df522beace5b94fb35b9586df58cbf6e45055c
edson@app100.com.br <mueledson>
2023-09-23 17:22:17 -0300

    projeto ja ta pronto so falta ver o video

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ShowProducts } from "./components/ShowProducts"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowProducts />}> </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
